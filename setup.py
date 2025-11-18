eb9d54974036ad673b039baa55eedf5396f9a981
Davide Setti <setti@spaziodati.eu>
2025-11-18 15:30:26 +0100

    remove six from requirements and setup.py

--- setup.py
+++ setup.py
@@ -5,56 +5,59 @@ from setuptools import setup, find_packages
 from setuptools.extension import Extension
 
 
-USE_CYTHON = 'CYTHONIZE' in os.environ
-IS_PYPY = platform.python_implementation() == 'PyPy'
-ext = '.pyx' if USE_CYTHON else '.c'
+USE_CYTHON = "CYTHONIZE" in os.environ
+IS_PYPY = platform.python_implementation() == "PyPy"
+ext = ".pyx" if USE_CYTHON else ".c"
 try:
     import numpy as np
+
     include_dirs = [np.get_include()]
 except ImportError:
     include_dirs = []
 extensions = [
-    Extension("scrapely._htmlpage",
-              ["scrapely/_htmlpage%s" % ext],
-              include_dirs=include_dirs),
-    Extension("scrapely.extraction._similarity",
-              ["scrapely/extraction/_similarity%s" % ext],
-              include_dirs=include_dirs),
+    Extension(
+        "scrapely._htmlpage", ["scrapely/_htmlpage%s" % ext], include_dirs=include_dirs
+    ),
+    Extension(
+        "scrapely.extraction._similarity",
+        ["scrapely/extraction/_similarity%s" % ext],
+        include_dirs=include_dirs,
+    ),
 ]
 if USE_CYTHON and not IS_PYPY:
     from Cython.Build import cythonize
+
     extensions = cythonize(extensions)
 if IS_PYPY:
     extensions = []
 
 
 setup(
-    name='scrapely',
-    version='0.13.5',
-    license='BSD',
-    description='A pure-python HTML screen-scraping library',
-    author='Scrapy project',
-    author_email='info@scrapy.org',
-    url='https://github.com/scrapy/scrapely',
-    packages=find_packages(exclude=['tests', 'tests.*']),
+    name="scrapely",
+    version="0.13.5",
+    license="BSD",
+    description="A pure-python HTML screen-scraping library",
+    author="Scrapy project",
+    author_email="info@scrapy.org",
+    url="https://github.com/scrapy/scrapely",
+    packages=find_packages(exclude=["tests", "tests.*"]),
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Internet :: WWW/HTTP',
-        'Topic :: Text Processing :: Markup :: HTML',
+        "Development Status :: 5 - Production/Stable",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Text Processing :: Markup :: HTML",
     ],
-    install_requires=['numpy', 'w3lib', 'six'],
-    extras_require={
-        'speedup': ['cython']
-    },
+    install_requires=["numpy", "w3lib"],
+    extras_require={"speedup": ["cython"]},
     ext_modules=extensions,
 )
